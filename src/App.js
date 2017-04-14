1d5bf795632dacc0bc58d1a48bb7cfc2e32be704
Akin909 <akin.sowemimo@gmail.com>
2017-04-14 11:54:30 +0100

    Add empty product list components relates #3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import ProductList from './components/ProductsList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Welcome to the Community Market
+        <ProductList />
       </div>
     );
   }
